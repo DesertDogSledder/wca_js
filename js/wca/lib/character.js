5d4671bd5742476d659f20555f361054457de57b
DesertDogSledder <desertdogsledder@gmail.com>
2018-09-23 19:54:00 -0500

    More changes to races and the update function

--- js/wca/lib/character.js
+++ js/wca/lib/character.js
@@ -80,7 +80,7 @@ class Homeworld
             chi: (options.chi === undefined) ? 0 : options.chi,
             psi: (options.psionics === undefined) ? 0 : options.psionics
         };
-        this.available_skills = (options.available_skills === undefined) ? null : options.available_skills;
+        this.available_skills = (options.available_skills === undefined) ? [] : options.available_skills;
         this.skills = (options.skills === undefined) ? [] : options.skills;
     }
 }
@@ -144,63 +144,285 @@ function calc_max_dice_pool_size(career_grade) {
 }
 
 
-function calc_total_stats(character)
+function calc_stat_total(character)
 {
     // Deep copy
-    // stat_total = jQuery.extend(true, {}, this.stats);
     var stat_total = deep_copy(character.stats);
-    
-    // Object.entries(this.race.stats).forEach(
-    //     ([key, value]) => stat_total[key] += value
-    // );
-    // let i;
-    // let j;
-    // for (i = 0; i < race.items; i++)
-    // {
-
-    // }
-    // for stat, value in self.race['Stats'].items():
-    //     stat_total[stat] += value
-
-    // for stat, value in self.homeworld['Homeworld'].stats.items():
-    //     stat_total[stat] += value
-
-    // for career in self.career_track:
-    //     for stat, value in career['Stats'].items():
-    //         stat_total[stat] += value
-
-    // console.log(character.race)
 
     for (stat in character.race.stats)
     {
-        stat_total[stat] += character.race.stats[stat]
+        stat_total[stat] += character.race.stats[stat];
         // console.log(stat + character.race.stats[stat])
     }
 
     for (stat in character.homeworld.stats)
     {
-        stat_total[stat] += character.homeworld.stats[stat]
+        stat_total[stat] += character.homeworld.stats[stat];
     }
 
     for (career in character.career_track)
     {
         for (stat in character.career_track[career].stats)
         {
-            stat_total[stat] += character.career_track[career].stats[stat]
+            stat_total[stat] += character.career_track[career].stats[stat];
         }
     }
-
-    console.log(stat_total)
 
     return stat_total;
 }
 
 function calc_skill_total(character)
 {
-    skill_total = [{}]
+    let skill_total = {};
+    // Racial skill tally
     for (skill in character.race.skills)
     {
-        if (skill.name in skill_total) skill_total[skill.name] += skill.rank
-        else skill_total[skill.name] = skill.rank
+        if (skill in skill_total) skill_total[skill] += character.race.skills[skill];
+        else skill_total[skill] = character.race.skills[skill];
     }
+    // Homeworld skill tally
+    for (skill in character.homeworld.skills)
+    {
+        if (skill in skill_total) skill_total[skill] += character.homeworld.skills[skill];
+        else skill_total[skill] = character.homeworld.skills[skill];
+    }
+    // Career skill tally
+    for (career in character.career_track)
+    {
+        for (skill in character.career_track.skills)
+        {
+            if (skill in skill_total) skill_total[skill] += career.skills[skill];
+            else skill_total[skill] = career.skills[skill];    
+        }
+    }
+
+    let sorted_skill_total = sort_object(skill_total);
+    return sorted_skill_total;
+}
+
+function calc_exploit_total(character)
+{
+    let exploit_total = {};
+
+    for (let i=0; i<character.race.exploits.length; i++)
+    {
+        exploit_total[character.race.exploits[i].name] = character.race.exploits[i].desc;
+    }
+
+    let sorted_exploit_total = sort_object(exploit_total);
+    // console.log(sorted_exploit_total);
+    return sorted_exploit_total;
+}
+
+function calc_derived_stats(character)
+{
+    let derived_stats = {};
+    let stat_total = calc_stat_total(character)
+    let skill_total = calc_skill_total(character)
+
+    let max_dice_pool = calc_max_dice_pool_size(woin_character.career_track.length);
+    let str_dice_pool_size = calc_dice_pool_size(total_stats['str']);
+    let agi_dice_pool_size = calc_dice_pool_size(total_stats['agi']);
+    let end_dice_pool_size = calc_dice_pool_size(total_stats['end']);
+    let int_dice_pool_size = calc_dice_pool_size(total_stats['int']);
+    let wil_dice_pool_size = calc_dice_pool_size(total_stats['wil']);
+    let cha_dice_pool_size = calc_dice_pool_size(total_stats['cha']);
+
+    // Cap dice pools here
+    // let str_dice_pool = (str_dice_pool_size <= max_dice_pool) ? str_dice_pool_size : max_dice_pool;
+    // let agi_dice_pool = (agi_dice_pool_size <= max_dice_pool) ? agi_dice_pool_size : max_dice_pool;
+    // let end_dice_pool = (end_dice_pool_size <= max_dice_pool) ? end_dice_pool_size : max_dice_pool;
+    // let int_dice_pool = (int_dice_pool_size <= max_dice_pool) ? int_dice_pool_size : max_dice_pool;
+    // let wil_dice_pool = (wil_dice_pool_size <= max_dice_pool) ? wil_dice_pool_size : max_dice_pool;
+    // let cha_dice_pool = (cha_dice_pool_size <= max_dice_pool) ? cha_dice_pool_size : max_dice_pool;
+
+    // Don't cap dice pools here
+    let str_dice_pool = str_dice_pool_size;
+    let agi_dice_pool = agi_dice_pool_size;
+    let end_dice_pool = end_dice_pool_size;
+    let int_dice_pool = int_dice_pool_size;
+    let wil_dice_pool = wil_dice_pool_size;
+    let cha_dice_pool = cha_dice_pool_size;
+
+
+    ////////////
+    // HEALTH //
+    ////////////
+    let average_health = end_dice_pool + wil_dice_pool
+    let health_info = 'Roll END (' + end_dice_pool + 'd6) + WIL (' + wil_dice_pool + 'd6)';;
+
+    if ('hardy' in skill_total)
+    {
+        let hardy_dice_pool_size = calc_dice_pool_size(skill_total.hardy);
+        // Cap dice pool here
+        // let hardy_dice_pool = (hardy_dice_pool_size <= max_dice_pool) ? hardy_dice_pool_size : max_dice_pool;
+
+        // Don't cap dice pool here
+        let hardy_dice_pool = hardy_dice_pool_size;
+        health_info += ' + hardy (' + hardy_dice_pool + 'd6)';
+        average_health += hardy_dice_pool;
+    }
+
+    average_health = Math.ceil(average_health * 3.5);
+    if (average_health >= 10) health_info += ' (average = ' + average_health + ')';
+    else health_info += ' (average bumped up to minimum = 10)';
+
+    derived_stats['health'] = health_info;
+
+    ///////////
+    // SPEED //
+    ///////////
+    let base_speed = str_dice_pool + agi_dice_pool;
+    let speed = base_speed;
+    if ('running' in skill_total)
+    {
+        running_dice_pool_size = calc_dice_pool_size(skill_total['running'])
+        // Cap dice pool here
+        // running_dice_pool = (running_dice_pool_size <= max_dice_pool) ? running_dice_pool_size : max_dice_pool;
+
+        // Don't cap dice pool here
+        running_dice_pool = running_dice_pool_size;
+        
+        speed += running_dice_pool;
+    }
+
+    let climbing = base_speed;
+    let swimming = base_speed;
+    let zero_g = base_speed;
+    let high_g = base_speed;
+    let low_g = base_speed;
+
+    if ('climbing' in skill_total)
+    {
+        climbing_dice_pool_size
+        climbing += calc_dice_pool_size(skill_total['climbing'])
+
+    }
+    if 'swimming' in skill_total:
+        swimming += calc_dice_pool_size(skill_total['swimming'])
+    if 'zero-g' in skill_total:
+        zero_g += calc_dice_pool_size(skill_total['zero-g'])
+    if 'high-g' in skill_total:
+        high_g += calc_dice_pool_size(skill_total['high-g'])
+    if 'low-g' in skill_total:
+        low_g += calc_dice_pool_size(skill_total['low-g'])
+
+    climbing = math.ceil(climbing / 2)
+    swimming = math.ceil(swimming / 2)
+    zero_g = math.ceil(zero_g / 2)
+    high_g = math.ceil(high_g / 2)
+    low_g = math.ceil(low_g / 2)
+
+    derived_stats['Speed'] = speed
+    derived_stats['Climb'] = climbing
+    derived_stats['Swim'] = swimming
+    derived_stats['Zero-G'] = zero_g
+    derived_stats['High-G'] = high_g
+    derived_stats['Low-G'] = low_g
+
+    ########
+    # JUMP #
+    ########
+    derived_stats['Horizontal Jump Running'] = stat_total['AGI']*2
+    derived_stats['Horizontal Jump Standing'] = stat_total['AGI']
+    # Vertical jump values cannot exceed horizontal jump values
+    if stat_total['STR'] <= stat_total['AGI']:
+        vertical_jump_standing = stat_total['STR']
+        vertical_jump_running = stat_total['STR'] * 2
+    else:
+        vertical_jump_standing = stat_total['AGI']
+        vertical_jump_running = stat_total['AGI'] * 2
+    derived_stats['Vertical Jump Running'] = vertical_jump_running
+    derived_stats['Vertical Jump Standing'] =vertical_jump_standing
+
+    #########
+    # CARRY #
+    #########
+    if 'carry' in skill_total:
+        base_carry = (stat_total['STR'] + stat_total['END'] + skill_total['carry']) * 10
+    else:
+        base_carry = (stat_total['STR'] + stat_total['END']) * 10
+
+    if self.race['Size'] == 'large':
+        base_carry = math.ceil(base_carry * 1.5)
+    elif self.race['Size'] == 'enormous':
+        base_carry *= 2
+    elif self.race['Size'] == 'gigantic':
+        base_carry *= 4
+    elif self.race['Size'] == 'colossal':
+        base_carry *= 8
+    elif self.race['Size'] == 'titanic':
+        base_carry *= 16
+
+    derived_stats['Carry'] = '{} (before exploits)'.format(base_carry)
+
+    ##############
+    # INITIATIVE #
+    ##############
+    if 'tactics' in skill_total:
+        tactics_dice_pool = calc_dice_pool_size(skill_total['tactics'])
+    else:
+        tactics_dice_pool = 0
+    if 'reactions' in skill_total:
+        reactions_dice_pool = calc_dice_pool_size(skill_total['reactions'])
+    else:
+        reactions_dice_pool = 0
+
+    derived_stats['Initiative'] = int_dice_pool + max([tactics_dice_pool, reactions_dice_pool])
+
+    ############
+    # DEFENSES #
+    ############
+    derived_stats['Melee Defense'] = max([str_dice_pool, agi_dice_pool])
+    derived_stats['Ranged Defense'] = agi_dice_pool
+    derived_stats['Mental Defense'] = max([cha_dice_pool, wil_dice_pool])
+    derived_stats['Vital Defense'] = end_dice_pool
+
+    if self.defense_skills['Melee'] != '' and self.defense_skills['Melee'] in skill_total:
+        derived_stats['Melee Defense'] += calc_dice_pool_size(skill_total[self.defense_skills['Melee']])
+    if self.defense_skills['Ranged'] != '' and self.defense_skills['Ranged'] in skill_total:
+        derived_stats['Ranged Defense'] += calc_dice_pool_size(skill_total[self.defense_skills['Ranged']])
+    if self.defense_skills['Mental'] != '' and self.defense_skills['Mental'] in skill_total:
+        derived_stats['Mental Defense'] += calc_dice_pool_size(skill_total[self.defense_skills['Mental']])
+    if self.defense_skills['Vital'] != '' and self.defense_skills['Vital'] in skill_total:
+        derived_stats['Vital Defense'] += calc_dice_pool_size(skill_total[self.defense_skills['Vital']])
+
+    derived_stats['Melee Defense'] = math.ceil(derived_stats['Melee Defense'] * 3.5)
+    derived_stats['Ranged Defense'] = math.ceil(derived_stats['Ranged Defense'] * 3.5)
+    derived_stats['Mental Defense'] = math.ceil(derived_stats['Mental Defense'] * 3.5)
+    derived_stats['Vital Defense'] = math.ceil(derived_stats['Vital Defense'] * 3.5)
+
+    if self.race['Size'] == 'tiny':
+        derived_stats['Melee Defense'] += 4
+        derived_stats['Ranged Defense'] += 4
+    elif self.race['Size'] == 'small':
+        derived_stats['Melee Defense'] += 2
+        derived_stats['Ranged Defense'] += 2
+    elif self.race['Size'] == 'large':
+        derived_stats['Melee Defense'] -= 4
+        derived_stats['Ranged Defense'] -= 4
+    elif self.race['Size'] == 'enormous':
+        derived_stats['Melee Defense'] -= 8
+        derived_stats['Ranged Defense'] -= 8
+    elif self.race['Size'] == 'gigantic':
+        derived_stats['Melee Defense'] -= 16
+        derived_stats['Ranged Defense'] -= 16
+    elif self.race['Size'] == 'colossal':
+        derived_stats['Melee Defense'] -= 32
+        derived_stats['Ranged Defense'] -= 32
+    elif self.race['Size'] == 'titantic':
+        # extrapolation based on previous trends
+        derived_stats['Melee Defense'] -= 64
+        derived_stats['Ranged Defense'] -= 64
+
+    if derived_stats['Melee Defense'] < 10:
+        derived_stats['Melee Defense'] = 10
+    if derived_stats['Ranged Defense'] < 10:
+        derived_stats['Ranged Defense'] = 10
+    if derived_stats['Mental Defense'] < 10:
+        derived_stats['Mental Defense'] = 10
+    if derived_stats['Vital Defense'] < 10:
+        derived_stats['Vital Defense'] = 10
+
+    return derived_stats
 }
--- js/wca/lib/helpers.js
+++ js/wca/lib/helpers.js
@@ -7,5 +7,26 @@ function deep_copy(original)
         ),
         original
       );
-      return copied;
+    return copied;
+}
+
+function sort_object(orig_obj)
+{
+    let keys = Object.keys(orig_obj);
+    keys.sort();
+
+    let sorted_obj = {};
+    let i;
+
+    for (i=0; i<keys.length; i++)
+    {
+        sorted_obj[keys[i]] = 0;
+    }
+
+    for (key in sorted_obj)
+    {
+        sorted_obj[key] = orig_obj[key];
+    }
+
+    return sorted_obj;
 }
